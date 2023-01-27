0388cca26f04c92c7f30370c6bb6a6072da34015
RuslanUchan <ruslanuchan97@gmail.com>
2023-01-27 22:33:02 +0700

    bug fix: correct status inclusion and comparison (1) include COMPLETED and PROGRESS the correct way (by parentheses) (2) cast score to integer for correct comparison (view give no data     type)

--- src/app.py
+++ src/app.py
@@ -21,7 +21,8 @@ media_list_query = cur.execute(
 		ROUND(AVG(appropriate_score), 2) AS avg_score,
 		COUNT(1) AS audience_count
 	FROM v_appropriate_score
-	WHERE status = 'COMPLETED' OR (status = 'CURRENT' AND progress >= 5)
+	WHERE (status = 'COMPLETED' OR (status = 'CURRENT' AND progress >= 5)) -- rules
+		AND CAST(appropriate_score AS INTEGER) > 0 -- don't calculate non-rating
 	GROUP BY title, media_type
 	HAVING COUNT(1) >= 5
 	ORDER BY 3 DESC, 4 DESC, 1 DESC
