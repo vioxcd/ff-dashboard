278f58b0cd49a46713a6b6f8a1d21415d5db846f
RuslanUchan <ruslanuchan97@gmail.com>
2023-02-01 19:48:46 +0700

    favorites query/view (p90)

---
+++ sqls/favourites_p90_view.sql
@@ -0,0 +1,23 @@
+CREATE VIEW v_favorites_p90 AS
+WITH
+counted_favs AS (
+	SELECT
+		name,
+		type,
+		COUNT(1) AS counts
+	FROM favourites
+	GROUP BY 1, 2
+	ORDER BY 3 DESC
+), ordered_per_type AS (
+	SELECT
+		name,
+		type,
+		counts,
+		1 - PERCENT_RANK()
+			OVER(PARTITION BY type ORDER BY counts DESC)
+			AS pct_rank
+	FROM counted_favs
+)
+SELECT *
+FROM ordered_per_type
+WHERE pct_rank > .9
