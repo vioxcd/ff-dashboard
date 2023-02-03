e67dd079725996c855bddb11c98eca03e1a75368
RuslanUchan <ruslanuchan97@gmail.com>
2023-02-03 20:50:08 +0700

    fix broken AOTY query

--- sqls/view_aoty_2022.sql
+++ sqls/view_aoty_2022.sql
@@ -1,10 +1,19 @@
 CREATE VIEW v_aoty_2022
 AS
-SELECT md.*, ar.avg_score AS ff_score, ar.audience_count
+SELECT
+  md.*,
+  ar.anichan_score,
+  ar.ff_score,
+  ar.audience_count
 FROM v_as_rules ar
 JOIN media_details md
 USING (media_id)
-WHERE avg_score >= 80.0
+WHERE anichan_score >= 80.0
+  AND ff_score >= 80.0
   AND media_type = 'ANIME'
   AND season_year = 2022
-ORDER BY ff_score DESC, audience_count DESC, title DESC
+ORDER BY
+  anichan_score DESC,
+  ff_score DESC,
+  audience_count DESC,
+  title DESC
