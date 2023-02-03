338c8fe3f75c707eef442d74a934a6adfb5e41b8
RuslanUchan <ruslanuchan97@gmail.com>
2023-02-03 20:34:13 +0700

    include anichan_score when in final view

--- sqls/view_as_rules.sql
+++ sqls/view_as_rules.sql
@@ -4,10 +4,13 @@ SELECT
 	media_id,
 	title,
 	media_type,
-	ROUND(AVG(appropriate_score), 2) AS avg_score,
+	CAST(AVG(scores_anichan) AS INTEGER) AS anichan_score,
+	CAST(AVG(appropriate_score) AS INTEGER) AS ff_score,
 	COUNT(1) AS audience_count
 FROM v_appropriate_score
-WHERE (status = 'COMPLETED' OR (status = 'CURRENT' AND progress >= 5)) -- rules: completed or at least 5 eps progress
+WHERE (status = 'COMPLETED' OR (status IN ('CURRENT', 'PAUSED') AND progress >= 5)) -- rules: completed or at least 5 eps progress
+	AND CAST(scores_anichan AS INTEGER) > 0 -- don't calculate non-rating
 	AND CAST(appropriate_score AS INTEGER) > 0 -- don't calculate non-rating
 GROUP BY title, media_type
 HAVING COUNT(1) >= 5 -- rules: minimum watched by 5 members
+-- ORDER BY 3 DESC, 4 DESC, 5 DESC, 1 DESC  -- ordering
