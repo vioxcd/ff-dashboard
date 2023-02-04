c17710a5b6795d5bbebd71f7106f5462c3df109b
RuslanUchan <ruslanuchan97@gmail.com>
2023-02-04 20:26:55 +0700

    put AOTY queries alongside its views previously it's on its own sql script; putting the query here reflects how these queries and views are closely related

--- sqls/view_aoty_2022.sql
+++ sqls/view_aoty_2022.sql
@@ -1,19 +1,79 @@
 CREATE VIEW v_aoty_2022
 AS
-SELECT
-  md.*,
-  ar.anichan_score,
-  ar.ff_score,
-  ar.audience_count
-FROM v_as_rules ar
-JOIN media_details md
-USING (media_id)
-WHERE anichan_score >= 80.0
-  AND ff_score >= 80.0
-  AND media_type = 'ANIME'
-  AND season_year = 2022
-ORDER BY
-  anichan_score DESC,
-  ff_score DESC,
-  audience_count DESC,
-  title DESC
+WITH
+aoty_2022 AS (
+  SELECT
+    md.*,
+    ar.anichan_score,
+    ar.ff_score,
+    ar.audience_count
+  FROM v_as_rules ar
+  JOIN media_details md
+  USING (media_id)
+  WHERE anichan_score >= 80.0
+    AND ff_score >= 80.0
+    AND media_type = 'ANIME'
+    AND season_year = 2022
+  ORDER BY
+    anichan_score DESC,
+    ff_score DESC,
+    audience_count DESC,
+    title DESC
+),
+aoty AS (
+	SELECT "AOTY" AS award, 1 AS award_order,
+		* FROM aoty_2022 ORDER BY ff_score DESC LIMIT 1
+),
+-- anime of the season (winter): my dress up darling
+aots_winter AS (
+	SELECT "AOTS Winter" AS award, 2 AS award_order,
+		* FROM aoty_2022 WHERE season = "WINTER" ORDER BY ff_score DESC LIMIT 1
+),
+-- anime of the season (spring): kaguya sama: love is war -ultra romantic-
+aots_spring AS (
+	SELECT "AOTS Spring" AS award, 3 AS award_order,
+		* FROM aoty_2022 WHERE season = "SPRING" ORDER BY ff_score DESC LIMIT 1
+),
+-- anime of the season (summer): made in abyss: the golden city of the scorching sun
+aots_summer AS (
+	SELECT "AOTS Summer" AS award, 4 AS award_order,
+		* FROM aoty_2022 WHERE season = "SUMMER" ORDER BY ff_score DESC LIMIT 1
+),
+-- anime of the season (fall): mob psycho iii
+aots_fall AS (
+	SELECT "AOTS Fall" AS award, 5 AS award_order,
+		* FROM aoty_2022 WHERE season = "FALL" ORDER BY ff_score DESC LIMIT 1
+),
+-- most popular: bocchi the rock!
+most_popular AS (
+	SELECT "Most Popular" AS award, 6 AS award_order,
+		* FROM aoty_2022 ORDER BY audience_count DESC LIMIT 1
+),
+-- best non-sequel: bocchi the rock!
+best_non_sequel AS (
+	SELECT "Best Non-Sequel" AS award, 7 AS award_order,
+		* FROM aoty_2022 WHERE is_sequel = 0 ORDER BY ff_score DESC LIMIT 1
+),
+-- best original: lycoris recoil
+best_original AS (
+	SELECT "Best Original" AS award, 8 AS award_order,
+		* FROM aoty_2022 WHERE source = "ORIGINAL" ORDER BY ff_score DESC LIMIT 1
+),
+-- best movie: drifting home
+best_movie AS (
+	SELECT "Best Movie" AS award, 9 AS award_order,
+		* FROM aoty_2022 WHERE format = "MOVIE" OR (format = "ONA" AND episodes = 1) ORDER BY ff_score DESC LIMIT 1
+)
+SELECT *
+FROM (
+	SELECT * FROM aoty
+	UNION SELECT * FROM aots_winter
+	UNION SELECT * FROM aots_spring
+	UNION SELECT * FROM aots_summer
+	UNION SELECT * FROM aots_fall
+	UNION SELECT * FROM most_popular
+	UNION SELECT * FROM best_non_sequel
+	UNION SELECT * FROM best_original
+	UNION SELECT * FROM best_movie
+)
+ORDER BY award_order
