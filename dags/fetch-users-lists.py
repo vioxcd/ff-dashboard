96c37bf392fbec909b2e10010f4f02ca003480c7
RuslanUchan <ruslanuchan97@gmail.com>
2023-03-30 00:38:06 +0700

    fix `fetch_user_favorites` bug and ensure dag runs - sqlite3 bindings error bugs because I use `append` instead of `extend`

--- dags/fetch-users-lists.py
+++ dags/fetch-users-lists.py
@@ -1,16 +1,33 @@
 import datetime as dt
 
-from custom.operators import AnilistFetchUserListOperator
+from custom.operators import (AnilistFetchUserFavoritesOperator,
+                              AnilistFetchUserListOperator)
 
 from airflow import DAG
+from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="fetch_users_details",
+    dag_id="fetch_users_lists",
     description="Fetches users' score format and lists from the Anilist API using a custom operator.",
-    start_date=dt.datetime(2023, 3, 3),
-    end_date=dt.datetime(2023, 3, 4),
+    start_date=dt.datetime(2023, 3, 30),
+    end_date=dt.datetime(2023, 3, 31),
     schedule_interval="@daily",
 ) as dag:
-    AnilistFetchUserListOperator(
-        task_id="fetch_user_details",
+    start = EmptyOperator(
+        task_id='start'
     )
+
+    end = EmptyOperator(
+        task_id='end'
+    )
+
+    # fetch_user_lists = AnilistFetchUserListOperator(
+    #     task_id="fetch_user_lists",
+    # )
+
+    fetch_user_favorites = AnilistFetchUserFavoritesOperator(
+        task_id="fetch_user_favorites",
+    )
+
+    # start >> fetch_user_lists >> fetch_user_favorites >> end
+    start >> fetch_user_favorites >> end
