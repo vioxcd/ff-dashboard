bad8095781597e7bef0d29d1b0a05c0178e59a6f
RuslanUchan <ruslanuchan97@gmail.com>
2023-04-09 19:00:35 +0700

    feat: add top anime and manga models

---
+++ sql_transforms/models/final/final_top_manga.sql
@@ -0,0 +1,5 @@
+{{ config(tags='final') }}
+
+SELECT *
+FROM {{ ref('int_media__section_and_ranking') }}
+WHERE media_type == "MANGA"
---
+++ sql_transforms/models/intermediate/int_media__section_and_ranking.sql
@@ -0,0 +1,58 @@
+{{ config(tags=["intermediate"]) }}
+
+WITH
+get_media_lists AS (
+	SELECT
+		ar.*,
+		md.season,
+		md.season_year,
+		md.episodes,
+		md.format,
+		md.average_score,
+		md.mean_score,
+		md.source,
+		md.studios,
+		md.is_sequel,
+		md.cover_image_url_xl AS cover_image_url
+	FROM int_media__as_rules ar
+	JOIN media_details md
+		USING (media_id)
+),
+
+get_sections AS (
+	SELECT *,
+		CASE
+			WHEN anichan_score >= 90 OR ff_score >= 90
+				THEN "gold"
+			WHEN (anichan_score > 85 AND anichan_score < 90)
+				OR (ff_score > 85 AND ff_score < 90)
+				THEN "silver"
+			WHEN anichan_score = 85 OR ff_score = 85
+				THEN "bronze"
+			ELSE "-"
+		END AS section
+	FROM get_media_lists
+),
+
+get_sections_rank AS (
+	SELECT *,	
+		ROW_NUMBER() OVER (PARTITION BY media_type
+							ORDER BY
+								CASE section
+									WHEN "gold" THEN 0
+									WHEN "silver" THEN 1
+									WHEN "bronze" THEN 2
+									ELSE 3
+								END,
+								anichan_score DESC,
+								ff_score DESC,
+								audience_count DESC,
+								title DESC
+							) AS ranking
+	FROM get_sections
+	WHERE section != "-"
+)
+
+SELECT *
+FROM get_sections_rank
+ORDER BY ranking
