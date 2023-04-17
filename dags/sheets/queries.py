928b9a2a695be12cd5a10cdcfc833a3425542ae2
RuslanUchan <ruslanuchan97@gmail.com>
2023-04-18 01:47:30 +0700

    fix: export cover image in aoty

--- dags/sheets/queries.py
+++ dags/sheets/queries.py
@@ -2,7 +2,8 @@ AOTY_2022_QUERY = """
                 SELECT
                     award_order, award, media_id, title, anichan_score,
                     ff_score, audience_count, season, season_year, media_type,
-                    format, source, studios, is_sequel
+                    format, source, studios, is_sequel,
+                    cover_image_url_xl AS cover_image_url
                 FROM final_aoty_2022
                 """
 
