0e59fb5c72af2f474eec07dfb3aa3b9f67e4ae9c
RuslanUchan <ruslanuchan97@gmail.com>
2023-04-26 02:29:07 +0700

    fix: missing `potentials` (JOIN-ed to missing `media_details`) create intermediate potential view without doing JOIN to `media_details` so that the table can be run in Airflow and the needed `media_details` can be downloaded afterwards

--- sql_transforms/models/final/final_potential.sql
+++ sql_transforms/models/final/final_potential.sql
@@ -2,8 +2,12 @@
 	tags=["final", "export", "dashboard"]
 ) }}
 
-SELECT *
-FROM {{ ref('int_media__potential') }}
+SELECT
+	p.*,
+	md.cover_image_url_xl AS cover_image_url
+FROM {{ ref('int_media__potential') }} p
+JOIN {{ source('ff_anilist', 'media_details') }} md
+	USING (media_id)
 WHERE
 	anichan_score >= 80
 	OR ff_score >= 80
