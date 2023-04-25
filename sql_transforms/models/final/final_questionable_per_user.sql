3824a69e2c42efdec1800144ed13fef11028ca87
RuslanUchan <ruslanuchan97@gmail.com>
2023-04-25 23:21:17 +0700

    fix: include all users in `questionable_per_user` some people think that the data is not complete because not all users are displayed in `questionable_per_user`, when in fact, it's because those users don't have any questionable ratings! therefore, add those users but mark them with NULL data to avoid future confusion

--- sql_transforms/models/final/final_questionable_per_user.sql
+++ sql_transforms/models/final/final_questionable_per_user.sql
@@ -2,7 +2,8 @@
 	tags=["final", "dashboard"]
 ) }}
 
-WITH ordered_score_diff AS (
+WITH
+ordered_score_diff AS (
 	SELECT
 		sd.*,
 		ROW_NUMBER() OVER (PARTITION BY username
@@ -12,8 +13,35 @@ WITH ordered_score_diff AS (
 	FROM {{ ref('int_media__score_diff') }} sd
 	JOIN {{ source('ff_anilist', 'media_details') }} md
 		USING (media_id)
+),
+
+users_with_no_questionable_titles AS (
+	SELECT
+		username,
+		-1 AS media_id,
+		"no questionable title" AS title,
+		"-" AS media_type,
+		-1 AS mean_score,
+		-1 AS audience_count,
+		-1 AS appropriate_score,
+		-1 AS score_diff,
+		-1 AS user_score_diff,
+		"-" AS cover_image_url
+	FROM {{ source('ff_anilist', 'users') }}
+	WHERE username NOT IN (
+		SELECT username FROM ordered_score_diff
+	)
+),
+
+result_set AS (
+	SELECT *
+	FROM ordered_score_diff
+	WHERE user_score_diff = 1  -- ' pick highest score_diff
 )
 
 SELECT *
-FROM ordered_score_diff
-WHERE user_score_diff = 1  -- ' pick highest score_diff
+FROM result_set
+UNION
+SELECT *
+FROM users_with_no_questionable_titles
+ORDER BY score_diff DESC
