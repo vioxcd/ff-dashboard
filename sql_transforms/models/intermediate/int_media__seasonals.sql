203170f24834a6307ab7c15cd22db6fba63fdf26
RuslanUchan <ruslanuchan97@gmail.com>
2023-04-26 18:24:21 +0700

    feat: include `n - 1` audience count in current and last season this feature are the result of my discussion and Mew
    specifically, it helps in determining which series that still has some
    hope to be included in the "Anime of the Season" award
    
    technically, it doesn't use `as_rules` anymore as it requires `n - 1`
    `audience_count`, therefore it copied the logic from `as_rules`. and
    then, it determines "last and current" season and take the series with
    at least `n - 1` audience (UNION-ing them with the normal "at least n")

--- sql_transforms/models/intermediate/int_media__seasonals.sql
+++ sql_transforms/models/intermediate/int_media__seasonals.sql
@@ -1,40 +1,129 @@
 {{ config(tags='intermediate') }}
 
 WITH
-seasonal_as_rules AS (
+calculated_minimal_users AS (
+	-- 20% rules
+	SELECT CAST(FLOOR(COUNT(1) * .2) AS INTEGER) AS minimal_user
+	FROM {{ source('ff_anilist', 'users') }}
+),
+
+counted_lists AS (
+	-- same as `as_rules, but include `n - 1`
 	SELECT
-		ar.*,
-		md.*,
+		media_id,
+		title,
+		media_type,
+		CAST(ROUND(AVG(anichan_score)) AS INTEGER) AS anichan_score,
+		CAST(ROUND(AVG(appropriate_score)) AS INTEGER) AS ff_score,
+		COUNT(1) AS audience_count
+	FROM {{ ref('stg_lists') }}
+	WHERE
+		(status = 'COMPLETED' OR (status IN ('CURRENT', 'PAUSED') AND progress >= 5))
+		AND anichan_score > 0
+		AND appropriate_score > 0
+		AND next_date IS NULL
+	GROUP BY media_id, media_type
+	HAVING COUNT(1) >= (
+		-- same as `as_rules`, but with -1 to include
+		-- ongoing series in last & current season
+		SELECT minimal_user - 1
+		FROM calculated_minimal_users
+	)
+),
+
+season_rules AS (
+	-- helps ordering the columns
+	SELECT
+		*,
 		CASE season
 			WHEN "WINTER" THEN 0
 			WHEN "SPRING" THEN 1
 			WHEN "SUMMER" THEN 2
 			ELSE 3
 		END AS season_code
-	FROM {{ ref('int_media__as_rules') }} ar
-		JOIN {{ source('ff_anilist', 'media_details') }} md
-		USING (media_id)
+	FROM {{ source('ff_anilist', 'media_details') }}
 	WHERE
 		season IS NOT NULL
 		AND season_year IS NOT NULL
 ),
 
-season_ranked AS (
+season_dim AS (
+	-- rank all season for determining `current and last season` later
 	SELECT
+		*,
+		ROW_NUMBER() OVER (ORDER BY
+							season_year DESC,
+							season_code DESC
+						) AS season_rank
+	FROM (
+		SELECT DISTINCT season, season_code, season_year
+		FROM season_rules
+	)
+),
+
+current_and_last_season AS (
+	SELECT
+		sd.*,
+		l.*,
+		md.cover_image_url_xl AS cover_image_url
+	FROM counted_lists l
+	JOIN {{ source('ff_anilist', 'media_details') }} md
+		USING (media_id)
+	JOIN season_dim sd
+		USING (season, season_year)
+	WHERE
+		season_rank IN (1, 2)
+		AND audience_count >= (
+			-- apply -1 to current and last
+			SELECT minimal_user - 1
+			FROM calculated_minimal_users
+		)
+),
+
+non_current_and_last_season AS (
+SELECT
+		sd.*,
+		l.*,
+		md.cover_image_url_xl AS cover_image_url
+	FROM counted_lists l
+	JOIN {{ source('ff_anilist', 'media_details') }} md
+		USING (media_id)
+	JOIN season_dim sd
+		USING (season, season_year)
+	WHERE
+		season_rank NOT IN (1, 2)
+		AND audience_count >= (
+			SELECT minimal_user
+			FROM calculated_minimal_users
+		)
+),
+
+all_seasons AS (
+	SELECT *
+	FROM current_and_last_season
+	UNION
+	SELECT *
+	FROM non_current_and_last_season
+),
+
+in_season_ranked AS (
+	SELECT
+		-- rank within its own season
 		ROW_NUMBER() OVER (PARTITION BY season_year, season_code
 							ORDER BY
-								anichan_score DESC,
 								ff_score DESC,
+								anichan_score DESC,
 								audience_count DESC
 							) AS in_season_rank,
 		*
-	FROM seasonal_as_rules
+	FROM all_seasons
 )
 
 SELECT
 	season_year,
 	season,
 	season_code,
+	season_rank,
 	in_season_rank,
 	media_id,
 	title,
@@ -42,6 +131,6 @@ SELECT
 	anichan_score,
 	ff_score,
 	audience_count,
-	cover_image_url_xl AS cover_image_url
-FROM season_ranked
+	cover_image_url
+FROM in_season_ranked
 ORDER BY season_year DESC, season_code DESC, ff_score DESC
