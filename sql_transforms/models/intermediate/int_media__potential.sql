5011c07cf7032c864f1d0c72c7f6aad7360e5f34
RuslanUchan <ruslanuchan97@gmail.com>
2023-04-27 06:15:04 +0700

    fix: dynamic potentials filter (minus 1 to 3)

--- sql_transforms/models/intermediate/int_media__potential.sql
+++ sql_transforms/models/intermediate/int_media__potential.sql
@@ -1,5 +1,13 @@
 {{ config(tags='intermediate') }}
 
+
+WITH
+calculated_minimal_users AS (
+	-- 20% rules
+	SELECT CAST(FLOOR(COUNT(1) * .2) AS INTEGER) AS minimal_user
+	FROM {{ source('ff_anilist', 'users') }}
+)
+
 SELECT
 	media_id,
 	title,
@@ -14,7 +22,11 @@ WHERE
 	AND appropriate_score > 0
 	AND next_date IS NULL  -- ' filter for current media
 GROUP BY media_id, media_type
-HAVING COUNT(1) IN (3, 4)  -- ' potential
+HAVING COUNT(1) IN (
+	(SELECT minimal_user - 1 FROM calculated_minimal_users),
+	(SELECT minimal_user - 2 FROM calculated_minimal_users),
+	(SELECT minimal_user - 3 FROM calculated_minimal_users)
+)  -- ' potential
 ORDER BY
 	ff_score DESC,
 	audience_count DESC
